df78cb955ad783f7bc2949f6c106c7160ec2db1a
ederjbbb <49681084+ederjbbb@users.noreply.github.com>
2021-05-04 16:03:29 +0100

    Update connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try{
         // mongodb connection string
-        const con = await mongoose.connect('mongodb+srv://ederjb:Pass2018390@cluster0.6ig72.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-', {
+        const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
