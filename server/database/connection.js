eaa0a96a01ee8f507ae471b810657285a9f42c90
eder <ederjb@icloud.com>
2021-05-04 15:21:38 +0100

    dotdev installed

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
     try{
         // mongodb connection string
-        const con = await mongoose.connect('mongodb link goes here', {
+        
+        const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
             useCreateIndex: true
+
         })
+        console.log(process.env.MONGO_URI+'MONFO IRIRIR');
 
         console.log(`MongoDB connected : ${con.connection.host}`);
     }catch(err){
