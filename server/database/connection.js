116a3f43d49e21858b1caa8edf9880777d61b4f0
eder <ederjb@icloud.com>
2021-05-04 14:07:26 +0100

    Final version

---
+++ server/database/connection.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try{
+        // mongodb connection string
+        const con = await mongoose.connect('mongodb+srv://ederjb:Pass2018390@cluster0.6ig72.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        })
+
+        console.log(`MongoDB connected : ${con.connection.host}`);
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
